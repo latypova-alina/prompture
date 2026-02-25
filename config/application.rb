9d52ba8717014f1a86050ee2c43f5604f3f5ad8d
Alina Latypova <alina.latypova@kyero.com>
2026-02-25 17:18:31 +0100

    fix semaphore

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,8 @@ module Prompture
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
-
+    config.autoload_paths += [Rails.root.join("lib")]
+    config.eager_load_paths += [Rails.root.join("lib")]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
