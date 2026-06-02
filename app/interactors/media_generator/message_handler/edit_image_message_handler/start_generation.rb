b7985c61e57b265facb398448d236e0ac8fc22f5
Alina Latypova <alina.latypova@kyero.com>
2026-06-02 16:42:10 +0200

    refactor

--- app/interactors/media_generator/message_handler/edit_image_message_handler/start_generation.rb
+++ app/interactors/media_generator/message_handler/edit_image_message_handler/start_generation.rb
@@ -4,18 +4,35 @@ module MediaGenerator
       class StartGeneration
         include Interactor
 
+        PROCESSOR = "nano_banana_edit_image".freeze
+
         delegate :command_request, to: :context
 
         def call
-          context.parent_request = command_request.latest_image_message
-          context.button_request = "nano_banana_edit_image"
+          prepare_context
 
-          create_result = MediaGenerator::ButtonHandler::CreateRequest.call(context)
           return context.fail!(error: create_result.error) if create_result.failure?
-
-          balance_result = MediaGenerator::ButtonHandler::DecrementBalance.call(context)
           return context.fail!(error: balance_result.error) if balance_result.failure?
 
+          send_generation_task
+        end
+
+        private
+
+        def prepare_context
+          context.parent_request = command_request.latest_image_message
+          context.button_request = PROCESSOR
+        end
+
+        def create_result
+          MediaGenerator::ButtonHandler::CreateRequest.call(context)
+        end
+
+        def balance_result
+          MediaGenerator::ButtonHandler::DecrementBalance.call(context)
+        end
+
+        def send_generation_task
           MediaGenerator::ButtonHandler::SendGenerationTask.call(context)
         end
       end
