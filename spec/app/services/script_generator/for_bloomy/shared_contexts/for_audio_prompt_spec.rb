3eff256089ae3a26f7bf51296a3cfacda7be3166
Alina Latypova <alina.r.latypova@gmail.com>
2026-07-23 16:50:30 +0200

    fix rubocop

--- spec/app/services/script_generator/for_bloomy/shared_contexts/for_audio_prompt_spec.rb
+++ spec/app/services/script_generator/for_bloomy/shared_contexts/for_audio_prompt_spec.rb
@@ -31,7 +31,8 @@ describe ScriptGenerator::ForBloomy::SharedContexts::ForAudioPrompt do
     it "posts script_text to the API" do
       request = instance_double(Faraday::Request)
       allow(request).to receive(:body=)
-      allow(connection).to receive(:post).with("/audio_prompt_for_bloomy_single_scene").and_yield(request).and_return(response)
+      allow(connection).to receive(:post).with("/audio_prompt_for_bloomy_single_scene")
+                                        .and_yield(request).and_return(response)
 
       audio_prompt_context.prompt
 
