a59fe74181222de914768fa1fa38be51d0b7790d
Alina Latypova <alina.r.latypova@gmail.com>
2026-08-05 15:21:19 +0200

    remove rails logger from bullet

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -1,6 +1,5 @@
 if Rails.env.development?
   Bullet.enable = true
-  Bullet.rails_logger = true
   Bullet.bullet_logger = true
   Bullet.n_plus_one_query_enable = true
   Bullet.unused_eager_loading_enable = false
